1ed832de77703d82837e5abff2f8e035bb367915
Rafal Wilinski <raf.wilinski@gmail.com>
2022-09-19 10:51:41 +0200

    chore: remove list from services menu

--- front/my-app/src/components/menus/ServicesMenu.tsx
+++ front/my-app/src/components/menus/ServicesMenu.tsx
@@ -6,30 +6,28 @@ export function ServicesMenu({ isDemo }: { isDemo?: boolean }) {
   const currentRegion = "us-east-1";
   return (
     <Command.Group heading="Services" style={{ marginTop: "10px" }}>
-      <Command.List>
-        {services.map((service) => {
-          return (
-            <Command.Item
-              key={`${service.ServiceName} ${service.ServiceShortName}`}
-              value={`${service.ServiceName} ${service.ServiceShortName}`}
-              onSelect={() => {
-                if (isDemo) {
-                  toast.success("Come on, this ain't real console!");
-                  return;
-                }
+      {services.map((service) => {
+        return (
+          <Command.Item
+            key={`${service.ServiceName} ${service.ServiceShortName}`}
+            value={`${service.ServiceName} ${service.ServiceShortName}`}
+            onSelect={() => {
+              if (isDemo) {
+                toast.success("Come on, this ain't real console!");
+                return;
+              }
 
-                if (service.ConsoleLink) {
-                  location.href = service.ConsoleLink;
-                } else {
-                  location.href = `https://${currentRegion}.console.aws.amazon.com/${service.ServiceShortName}/home?region=${currentRegion}#/functions`;
-                }
-              }}
-            >
-              {service.ServiceName}
-            </Command.Item>
-          );
-        })}
-      </Command.List>
+              if (service.ConsoleLink) {
+                location.href = service.ConsoleLink;
+              } else {
+                location.href = `https://${currentRegion}.console.aws.amazon.com/${service.ServiceShortName}/home?region=${currentRegion}#/functions`;
+              }
+            }}
+          >
+            {service.ServiceName}
+          </Command.Item>
+        );
+      })}
     </Command.Group>
   );
 }
