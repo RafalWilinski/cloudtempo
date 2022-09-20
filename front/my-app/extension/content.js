e99bf3030198d6791d23abf86bdccef8edcff5b1
Rafal Wilinski <raf.wilinski@gmail.com>
2022-09-20 13:24:08 +0200

    new ext

--- front/my-app/extension/content.js
+++ front/my-app/extension/content.js
@@ -1,5 +1,5 @@
 const stylesPath = "./dist/index.1001ba1e.css";
-const scriptPath = "./dist/index.a0ed1810.js";
+const scriptPath = "./dist/index.9662c0c4.js";
 const htmlBody = `<div id="aws-search-app"></div><div aws-search-extensionid="${chrome.runtime.id}"></div>`;
 
 document.body.insertAdjacentHTML("beforeend", htmlBody);
